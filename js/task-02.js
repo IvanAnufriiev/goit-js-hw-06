d498e27d9b251c55b97154ba49b3d5bfb5f26781
IvanAnufriiev <115418712+IvanAnufriiev@users.noreply.github.com>
2023-02-24 22:31:26 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list= document.querySelector(`#ingredients`);
+console.log(list);
+
+const elements = ingredients.forEach((element)=>{
+  const listEl = document.createElement(`li`);
+  listEl.classList.add(`item`);
+  listEl.textContent = `${element}`;
+  list.appendChild(listEl);
+  console.log(listEl);
+});
+
+
+
+
