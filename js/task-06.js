123e9a44649bce0d9e6c42a671db3a7e234c5617
IvanAnufriiev <115418712+IvanAnufriiev@users.noreply.github.com>
2023-02-26 19:59:47 +0100

    26.02

---
+++ js/task-06.js
@@ -0,0 +1,8 @@
+
+    const validationInput = document.querySelector("#validation-input");
+
+    validationInput.addEventListener("blur", () => {
+    validationInput.classList.toggle("valid", validationInput.value.length === 6 );
+
+    validationInput.classList.toggle("invalid" )
+})
