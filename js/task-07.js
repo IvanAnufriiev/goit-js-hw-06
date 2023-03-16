04b4d4168126e107c39d08fbe9fc11f1747ac8c8
IvanAnufriiev <115418712+IvanAnufriiev@users.noreply.github.com>
2023-03-16 18:46:30 +0100

    goit-js-hw-07

--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-const changeFontSize = document.querySelector('#font-size-control');
-const textSize = document.querySelector('#text');
-
-changeFontSize.addEventListener("input", (event) => {
-    let changeSize = event.target.value;
-    textSize.style.fontSize = `${changeSize}px`;
-});
